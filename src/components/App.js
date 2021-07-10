5f72f01b534c51cbe29c9368e83613862c69c66e
Ripu Sudan Soni <ripu7481@gmail.com>
2021-07-10 23:48:29 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,14 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [change, setChange] = useState(true);
   return (
-    <div id="main">
-      // Do not alter the main div
+    <>
+    <div>
+      <button onClick = {()=> setChange(!change)}>click</button>
+       {change?<p>para</p> : <p>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
     </div>
+    </>
   );
 }
 
