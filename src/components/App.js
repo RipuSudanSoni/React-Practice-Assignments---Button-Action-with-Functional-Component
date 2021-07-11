5698927b15ec5ff0fbd9dd36830f6b551b258bd0
Ripu Sudan Soni <ripu7481@gmail.com>
2021-07-11 20:20:58 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import "./../styles/App.css";
 
 function App() {
   const [changes, setChange] = useState(false);
-  const handler = () => {
+  const Eventhandler = () => {
     setChange(true);
   }
   const renderParaButton = () => {
@@ -12,45 +12,15 @@ function App() {
       
     }
     else {
-      return <button id="click" onClick = {handler}>click Me</button>
+      return <button id="click" onClick = {Eventhandler}>click Me</button>
     }
   }
 
   return (
     <div id="main">
-      {renderParaButton}
+      {renderParaButton()}
     </div>
   );
 }
 
-
 export default App;
-
-// import React, {Component, useState} from "react";
-// import "./../styles/App.css";
-
-// function App() {
-//   const[renderPara,setRenderPara]=useState(false);
-  
-//   const buttonClickHandler = () =>{
-//     setRenderPara(true);
-//   }
-
-//   const renderParaButton =()=>{
-//     if(renderPara){
-//       return <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
-
-//     }
-//     else{
-//       return <button id="click" onClick={buttonClickHandler}>Click Me</button>
-//     }
-//   }
-//   return (
-//     <div id="main">
-//       {renderParaButton()}
-//     </div>
-//   );
-// }
-
-
-// export default App;
