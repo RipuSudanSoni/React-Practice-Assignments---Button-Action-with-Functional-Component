1545111792df85ab3b33acaab6a5c9e6af263764
Ripu Sudan Soni <ripu7481@gmail.com>
2021-07-11 17:30:36 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,55 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
-  const [change, setChange] = useState(true);
+  const [changes, setChange] = useState(false);
+  const handler = () => {
+    setChange(true);
+  }
+  const renderParaButton = () => {
+    if(changes) {
+      return <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+      
+    }
+    else {
+      return <button id="click" onClick = {handler}>click Me</button>
+    }
+  }
+
   return (
-    <>
-    <div>
-      <button onClick = {()=> setChange(!change)}>click</button>
-       {change?<p>para</p> : <p>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
+    <div id="main">
+      {renderParaButton}
     </div>
-    </>
   );
 }
 
 
 export default App;
+
+// import React, {Component, useState} from "react";
+// import "./../styles/App.css";
+
+// function App() {
+//   const[renderPara,setRenderPara]=useState(false);
+  
+//   const buttonClickHandler = () =>{
+//     setRenderPara(true);
+//   }
+
+//   const renderParaButton =()=>{
+//     if(renderPara){
+//       return <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+
+//     }
+//     else{
+//       return <button id="click" onClick={buttonClickHandler}>Click Me</button>
+//     }
+//   }
+//   return (
+//     <div id="main">
+//       {renderParaButton()}
+//     </div>
+//   );
+// }
+
+
+// export default App;
